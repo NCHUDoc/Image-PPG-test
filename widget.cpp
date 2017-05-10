9e873cad0d1c5602d27d6b7887adf2434638488c
mingfan.wei <mingfan.wei@gmail.com>
2017-05-10 16:40:13 +0800

    2017050902 Read a byte
    ==========================
    1. Read a byte from yuv file.
    
    Results:
    1. Seems not right, result is 4 and -1 but binary file shows AA.
    -> Read one byte from file new000.yuv:  -1

--- widget.cpp
+++ widget.cpp
@@ -4,20 +4,25 @@
 #include <QIODevice>
 #include <QFile>
 #include <QtCore>
+#include <QDebug>
 
+//unsigned char y_color[480][640];
+char *y_color = new char[10];
+unsigned char cb_img[240][320];
+unsigned char cr_img[240][320];
 
 widget::widget(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::widget)
 {
     ui->setupUi(this);
-    QFile file("out.txt");
-    file.open(QIODevice::Append);
-    QDataStream out(&file);
-    out << QString("the answer is");   // serialize a string
-    out << (qint32)42;        // serialize an integer
-    //out << map;
+    QFile file("new000.yuv");
+    file.open(QIODevice::ReadOnly);
+    //QDataStream data(&file);
+    qint8 bytes = file.read(static_cast<char *>(y_color), sizeof(y_color));
+
     file.close();
+    qDebug()<<"Read one byte from file new000.yuv: "<<bytes;
 //    video_ptr = fopen("C:/Users/User/git/Imag-PPG-test/zz05.yuv","rb");
 //    frame = new QImage(rgb_buffer,640,480,QImage::Format_RGB888);
 }
