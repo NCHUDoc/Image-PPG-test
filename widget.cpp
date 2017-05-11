c3de56ae58c8cfb6b38c2e3bafa4daabaf76e27d
mingfan.wei <mingfan.wei@gmail.com>
2017-05-11 19:16:49 +0800

    2017051102 Read 5 bytes
    ==========================
    1. Read 5 bytes from yuv file.
    2. Use fread function.
    
    Results:
    1. result is 
    Read one byte from file new000.yuv:  170 
    Read one byte from file new000.yuv:  170 
    Read one byte from file new000.yuv:  170 
    Read one byte from file new000.yuv:  170 
    Read one byte from file new000.yuv:  168 
    Read one byte from file new000.yuv:  168 
    -> Same with binary file 'new000.yuv' y first 5 bytes. AA AA AA AA A8

--- widget.cpp
+++ widget.cpp
@@ -5,31 +5,59 @@
 #include <QFile>
 #include <QtCore>
 #include <QDebug>
+#include <stdio.h>
+#include <stdlib.h>
 
-//unsigned char y_color[480][640];
-char *y_color = new char[10];
+#define X_SIZE 640 //640*480 sequence
+#define Y_SIZE 480 //640*480 sequence
+int ONE_SIZE= X_SIZE* Y_SIZE;
+FILE *video_ptr;
+QImage *frame;
+unsigned char y_color[480][640];
+//char *y_color = new char[10];
 unsigned char cb_img[240][320];
 unsigned char cr_img[240][320];
+unsigned char rgb_buffer[640*480*3];
+unsigned char  yuv_buffer_pointer[640*480*3];
 
 widget::widget(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::widget)
 {
     ui->setupUi(this);
-    QFile file("new000.yuv");
+    //QFile file("new000.yuv");
+    video_ptr = fopen("new000.yuv","rb");
+    frame = new QImage(rgb_buffer,640,480,QImage::Format_RGB888);
     //QFile file("out.txt");
     //QString bytes = "";
-    qint64 bytes;
-    file.open(QIODevice::ReadOnly);
+    //qint64 bytes;
+    //file.open(QIODevice::ReadOnly);
     //QByteArray bytes = file.readAll();
+    fread(yuv_buffer_pointer, sizeof(unsigned char), ONE_SIZE*2, video_ptr);
 
-    QDataStream data(&file);
+    ///////////////422-420
+    for(int y=0;y<480;y++)
+        for(int x=0;x<640;x++)
+            y_color[y][x]=yuv_buffer_pointer[2*(x+y*640)];
+
+    for(int y=0;y<240;y++,y++)
+        for(int x=0;x<320;x++,x++)
+            {
+                cb_img[y][x]=yuv_buffer_pointer[(x+y*640)*4+1];
+                cr_img[y][x]=yuv_buffer_pointer[(x+y*640)*4+3];
+            }
+    //QDataStream data(&file);
     //qint8 bytes = file.read(static_cast<char *>(y_color), sizeof(y_color));
     //qint16 bytes;
-    data >> bytes;
+    //data >> bytes;
 
-    qDebug()<<"Read one byte from file new000.yuv: "<<bytes;
-    file.close();
+    qDebug()<<"Read one byte from file new000.yuv: "<<y_color[0][0];
+    qDebug()<<"Read one byte from file new000.yuv: "<<y_color[0][1];
+    qDebug()<<"Read one byte from file new000.yuv: "<<y_color[0][2];
+    qDebug()<<"Read one byte from file new000.yuv: "<<y_color[0][3];
+    qDebug()<<"Read one byte from file new000.yuv: "<<y_color[0][4];
+    qDebug()<<"Read one byte from file new000.yuv: "<<y_color[0][5];
+    //file.close();
 //    video_ptr = fopen("C:/Users/User/git/Imag-PPG-test/zz05.yuv","rb");
 //    frame = new QImage(rgb_buffer,640,480,QImage::Format_RGB888);
 }
